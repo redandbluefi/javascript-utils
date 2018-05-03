470520b06aaf705698e78a5ab2fde945b87c28c5
Hannu Kärkkäinen <hannu@redandblue.fi>
2018-05-03 10:45:15 +0300

    Updated tests

--- src/flattenObject.spec.js
+++ src/flattenObject.spec.js
@@ -5,24 +5,27 @@ describe('flattenObject', () => {
     const translations = {
       header: {
         tagline: {
-          kiniseko: '[Ki Niseko] Stay. Play. Experience.',
-          nisekocentral: '[Niseko Central] Stay. Play. Experience.'
+          test1: 'Hello world #1',
+          test2: 'Hello world #2'
         }
       },
       payment: {
-        veritrans: {
+        errors: {
           Q001: 'Something was wrong with the credit card details: {message}',
           Q002: 'Invalid credit card details. {message}',
-          Q099: 'Error occurred on the payment service provide. Please wait a while and try again. If the problem persist, please contact us.'
+          Q099:
+            'Error occurred on the payment service provide. Please wait a while and try again. If the problem persist, please contact us.'
         }
       }
-    }
+    };
     const EXPECTED_FLATTENED = {
-      'header.tagline.kiniseko': '[Ki Niseko] Stay. Play. Experience.',
-      'header.tagline.nisekocentral': '[Niseko Central] Stay. Play. Experience.',
-      'payment.veritrans.Q001': 'Something was wrong with the credit card details: {message}',
-      'payment.veritrans.Q002': 'Invalid credit card details. {message}',
-      'payment.veritrans.Q099': 'Error occurred on the payment service provide. Please wait a while and try again. If the problem persist, please contact us.'
+      'header.tagline.test1': 'Hello world #1',
+      'header.tagline.test2': 'Hello world #2',
+      'payment.errors.Q001':
+        'Something was wrong with the credit card details: {message}',
+      'payment.errors.Q002': 'Invalid credit card details. {message}',
+      'payment.errors.Q099':
+        'Error occurred on the payment service provide. Please wait a while and try again. If the problem persist, please contact us.'
     };
 
     let result = flattenObject(translations);
